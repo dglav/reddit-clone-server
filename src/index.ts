31fdd43f0f4afdd3da9556f55f44f0fcf963621c
Drew <glavich_drew@yahoo.com>
2021-01-10 17:41:30 +0900

    feat: added user resolver and set cookie

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import "reflect-metadata";
 import express from "express";
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import microOrmConfig from "./mikro-orm.config";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
+import session from "express-session";
+import redis from "redis";
+import connectRedis from "connect-redis";
+import { __prod__ } from "./constants";
+import { MyContext } from "./types";
+import microOrmConfig from "./mikro-orm.config";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
@@ -15,12 +19,35 @@ const main = async () => {
 
   const app = express();
 
+  // Configure Redis
+  // Needs to come before ApolloServer because that will use information from Redis
+  // (the order of execution matters)
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // csrf
+        secure: __prod__, // only works in https
+      },
+      saveUninitialized: false,
+      secret: "ailfbleiwbfliaebf",
+      resave: false,
+    })
+  );
+
+  // Configure Apollo Server
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
   apolloServer.applyMiddleware({ app });
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -84,7 +84,7 @@ export class UserResolver {
   @Mutation(() => UserResponse)
   async login(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, {
       username: options.username,
@@ -100,6 +100,9 @@ export class UserResolver {
         errors: [{ field: "password", message: "incorrect password" }],
       };
     }
+
+    req.session.userId = user.id;
+
     return { user };
   }
 }
