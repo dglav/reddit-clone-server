b98ba6d6527f5d7bd7955872ae76222b96d076ee
Drew <glavich_drew@yahoo.com>
2021-07-09 23:05:04 +0900

    feat: added user data in posts query

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -50,17 +50,30 @@ export class PostResolver {
   ): Promise<PaginatedPosts> {
     const realLimit = Math.min(50, limit);
     const realLimitPlusOne = Math.min(50, limit) + 1;
-    const queryBuilder = getConnection()
-      .getRepository(Post)
-      .createQueryBuilder("post")
-      .orderBy('"createdAt"', "DESC")
-      .take(realLimitPlusOne);
+
+    const replacements: any[] = [realLimitPlusOne];
     if (cursor) {
-      queryBuilder.where('"createdAt" < :cursor', {
-        cursor: new Date(parseInt(cursor)),
-      });
+      replacements.push(new Date(parseInt(cursor)));
     }
-    const posts = await queryBuilder.getMany();
+
+    const posts = await getConnection().query(
+      `
+      SELECT p.*, 
+      json_build_object(
+        'id', u.id,
+        'username', u.username,
+        'email', u.email,
+        'createdAt', u."createdAt",
+        'updatedAt', u."updatedAt"
+        ) creator
+      FROM post p
+      INNER JOIN public.user u on u.id = p."creatorId"
+      ${cursor ? `WHERE p."createdAt" < $2` : ""}
+      ORDER BY p."createdAt" DESC
+      LIMIT $1
+    `,
+      replacements
+    );
 
     return {
       posts: posts.slice(0, realLimit),
